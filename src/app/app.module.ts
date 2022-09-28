0dfd27618f00a139dffe63285c530788904e341b
Jose Ramos <jomaramos4@gmail.com>
2022-09-28 13:16:30 -0700

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,14 +21,16 @@ import { PopupsModule } from './shared/popups';
 
 import { NotificationModule } from './services';
 
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -51,7 +53,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    HeaderComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
