134c6c32f33b846f0e8e65df24b29be28e102f71
Jose Ramos <jomaramos4@gmail.com>
2022-09-28 13:03:58 -0700

    restore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  name: 'dev',
+  firebase:{
+    config:{
+      apiKey: "AIzaSyAnF0a2O9Y9xzDtGQu05ykX8pBSt3ss_1Y",
+      authDomain: "edificacion-app-6bb73.firebaseapp.com",
+      projectId: "edificacion-app-6bb73",
+      storageBucket: "edificacion-app-6bb73.appspot.com",
+      messagingSenderId: "1017179083683",
+      appId: "1:1017179083683:web:10c483d58ccb6052abc016"
+    }
+  }
 };
 
 /*
